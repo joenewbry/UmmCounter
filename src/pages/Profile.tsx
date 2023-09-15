3738f982ad7cd11d99414b2beb55ca5ceb4ccb07
Evan <evanscribner1@gmail.com>
2023-09-15 15:38:18 -0400

    add firebase SDK and upload file

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,4 +1,58 @@
+import { collection, getDocs } from "firebase/firestore/lite";
+import { app, db } from "../firebase";
+import { useEffect, useState } from "react";
+
+import { getStorage, ref, uploadBytes } from "firebase/storage";
+
+const storage = getStorage(app);
+// todo: this needs to include the full file name and extension
+const storageRef = ref(storage, "transcripts/evan/");
+
+async function getTranscripts(db: any) {
+  const transcriptsCollection = collection(db, "transcripts");
+  const transcriptsSnapshot = await getDocs(transcriptsCollection);
+  const cityList = transcriptsSnapshot.docs.map((doc) => doc.data());
+  return cityList;
+}
+
 const Profile = () => {
-  return <div>Profile</div>;
+  useEffect(() => {
+    getTranscripts(db).then((data) => console.log(data));
+  }, []);
+
+  //   // 'file' comes from the Blob or File API
+  // uploadBytes(storageRef, file).then((snapshot) => {
+  //   console.log('Uploaded a blob or file!');
+  // });
+
+  // file input to upload a recording to your profile
+
+  const [file, setFile] = useState<File | null>(null);
+
+  return (
+    <div>
+      Profile
+      <input
+        type="file"
+        onChange={(e) => {
+          if (e.target.files) {
+            setFile(e.target.files[0]);
+          }
+        }}
+      />
+      <button
+        disabled={!file}
+        onClick={async () => {
+          if (file) {
+            const buffer = await file.arrayBuffer();
+            const snapshot = await uploadBytes(storageRef, buffer);
+            console.log("Uploaded a blob or file!", snapshot);
+          }
+        }}
+      >
+        Upload
+      </button>
+    </div>
+  );
 };
 export default Profile;
