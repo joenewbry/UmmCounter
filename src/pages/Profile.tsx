8f7abc3f53bef07be355f6c8b983c743a300d123
Evan <evanscribner1@gmail.com>
2023-09-15 17:18:42 -0400

    fix file naming on upload

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -6,26 +6,18 @@ import { getStorage, ref, uploadBytes } from "firebase/storage";
 
 const storage = getStorage(app);
 // todo: this needs to include the full file name and extension
-const storageRef = ref(storage, "transcripts/evan/");
 
 async function getTranscripts(db: any) {
   const transcriptsCollection = collection(db, "transcripts");
   const transcriptsSnapshot = await getDocs(transcriptsCollection);
-  const cityList = transcriptsSnapshot.docs.map((doc) => doc.data());
-  return cityList;
+  const transcriptionList = transcriptsSnapshot.docs.map((doc) => doc.data());
+  return transcriptionList;
 }
 
 const Profile = () => {
   useEffect(() => {
     getTranscripts(db).then((data) => console.log(data));
   }, []);
-
-  //   // 'file' comes from the Blob or File API
-  // uploadBytes(storageRef, file).then((snapshot) => {
-  //   console.log('Uploaded a blob or file!');
-  // });
-
-  // file input to upload a recording to your profile
 
   const [file, setFile] = useState<File | null>(null);
 
@@ -43,7 +35,9 @@ const Profile = () => {
       <button
         disabled={!file}
         onClick={async () => {
-          if (file) {
+          if (file && file.name) {
+            // duplicate file names will be overwritten
+            const storageRef = ref(storage, `transcripts/evan/${file.name}`);
             const buffer = await file.arrayBuffer();
             const snapshot = await uploadBytes(storageRef, buffer);
             console.log("Uploaded a blob or file!", snapshot);
